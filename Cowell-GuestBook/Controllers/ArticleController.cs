3cece4362884dfffe87b9a3e47381ba7c08ab5a0
cowellart <art@cowell.com.tw>
2015-08-27 14:25:04 +0800

    Article CRUD

--- Cowell-GuestBook/Controllers/ArticleController.cs
+++ Cowell-GuestBook/Controllers/ArticleController.cs
@@ -76,23 +76,20 @@ namespace Cowell_GuestBook.Controllers {
             if (aRTICLE == null) {
                 return HttpNotFound();
             }
-            ViewBag.FORUM_ID = new SelectList(db.Forum, "ID", "TITLE", aRTICLE.FORUM_ID);
             return View(aRTICLE);
         }
 
-        // POST: Article/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FORUM_ID,TITLE,BODY,BUD_DTM,UPD_DTM,AUTHOR")] ARTICLE aRTICLE) {
-            if (ModelState.IsValid) {
-                db.Entry(aRTICLE).State = EntityState.Modified;
+        public ActionResult Edit(int id,FormCollection frm) {
+            var obj = db.ARTICLE.Find(id);
+            obj.UPD_DTM = DateTime.Now;
+            if (TryUpdateModel<ARTICLE>(obj, "", frm.AllKeys, new string[] { "BUD_DTM" })) {
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("index", "article", new { ForumID = obj.FORUM_ID });
             }
-            ViewBag.FORUM_ID = new SelectList(db.Forum, "ID", "TITLE", aRTICLE.FORUM_ID);
-            return View(aRTICLE);
+            return View(obj);
         }
 
         // GET: Article/Delete/5
@@ -111,10 +108,10 @@ namespace Cowell_GuestBook.Controllers {
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id) {
-            ARTICLE aRTICLE = db.ARTICLE.Find(id);
-            db.ARTICLE.Remove(aRTICLE);
+            ARTICLE obj = db.ARTICLE.Find(id);
+            db.ARTICLE.Remove(obj);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("index", "article", new { forumID = obj.FORUM_ID });
         }
 
         protected override void Dispose(bool disposing) {
