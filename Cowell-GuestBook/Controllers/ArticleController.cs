1d0f8bce509e2b294cd13bcc5050528ed20bff2d
cowellart <art@cowell.com.tw>
2015-08-27 14:01:55 +0800

    create article

--- Cowell-GuestBook/Controllers/ArticleController.cs
+++ Cowell-GuestBook/Controllers/ArticleController.cs
@@ -8,76 +8,72 @@ using System.Web;
 using System.Web.Mvc;
 using Cowell_GuestBook.Models;
 
-namespace Cowell_GuestBook.Controllers
-{
-    public class ArticleController : Controller
-    {
+namespace Cowell_GuestBook.Controllers {
+    public class ArticleController : Controller {
         private MVCTESTEntities db = new MVCTESTEntities();
 
-        public ActionResult Index(int? ForumID)
-        {
+        public ActionResult Index(int? ForumID) {
             IQueryable<ARTICLE> objs;
             if (ForumID == null) {
                 // Get: /Article
-                objs=db.ARTICLE.Include(a => a.Forum);
+                objs = db.ARTICLE.Include(a => a.Forum);
             } else {
                 // Get: /Article?forumID=12
-                objs=db.ARTICLE.Where(x=>x.FORUM_ID==ForumID).OrderBy(x=>x.BUD_DTM);
+                objs = db.ARTICLE.Where(x => x.FORUM_ID == ForumID).OrderBy(x => x.BUD_DTM);
                 ViewData["ForumDetailModel"] = db.Forum.Find(ForumID);
             }
             return View(objs.ToList());
         }
 
         // GET: Article/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             ARTICLE aRTICLE = db.ARTICLE.Find(id);
-            if (aRTICLE == null)
-            {
+            if (aRTICLE == null) {
                 return HttpNotFound();
             }
             return View(aRTICLE);
         }
 
         // GET: Article/Create
-        public ActionResult Create()
-        {
-            ViewBag.FORUM_ID = new SelectList(db.Forum, "ID", "TITLE");
+        public ActionResult Create(int? ForumID) {
+            //新建文章已經限定一定要從討論區才能新建
+            if (ForumID == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //驗證討論區是否存在
+            Forum f = db.Forum.Find(ForumID);
+            if (f == null) {
+                return HttpNotFound();
+            }
+            ViewData["ForumID"] = ForumID;
             return View();
         }
 
-        // POST: Article/Create
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,FORUM_ID,TITLE,BODY,BUD_DTM,UPD_DTM,AUTHOR")] ARTICLE aRTICLE)
-        {
-            if (ModelState.IsValid)
-            {
-                db.ARTICLE.Add(aRTICLE);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        public ActionResult Create(FormCollection frm) {
 
-            ViewBag.FORUM_ID = new SelectList(db.Forum, "ID", "TITLE", aRTICLE.FORUM_ID);
-            return View(aRTICLE);
+            ARTICLE obj = new ARTICLE();
+            obj.BUD_DTM = DateTime.Now;
+            obj.UPD_DTM = DateTime.Now;
+            if (TryUpdateModel<ARTICLE>(obj, "", frm.AllKeys, new string[] { })) {
+                db.ARTICLE.Add(obj);
+                db.SaveChanges();
+                return RedirectToAction("index", "article", new { forumID=obj.FORUM_ID});
+            }
+            return View(obj);
         }
 
         // GET: Article/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             ARTICLE aRTICLE = db.ARTICLE.Find(id);
-            if (aRTICLE == null)
-            {
+            if (aRTICLE == null) {
                 return HttpNotFound();
             }
             ViewBag.FORUM_ID = new SelectList(db.Forum, "ID", "TITLE", aRTICLE.FORUM_ID);
@@ -89,10 +85,8 @@ namespace Cowell_GuestBook.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FORUM_ID,TITLE,BODY,BUD_DTM,UPD_DTM,AUTHOR")] ARTICLE aRTICLE)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "ID,FORUM_ID,TITLE,BODY,BUD_DTM,UPD_DTM,AUTHOR")] ARTICLE aRTICLE) {
+            if (ModelState.IsValid) {
                 db.Entry(aRTICLE).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -102,15 +96,12 @@ namespace Cowell_GuestBook.Controllers
         }
 
         // GET: Article/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             ARTICLE aRTICLE = db.ARTICLE.Find(id);
-            if (aRTICLE == null)
-            {
+            if (aRTICLE == null) {
                 return HttpNotFound();
             }
             return View(aRTICLE);
@@ -119,18 +110,15 @@ namespace Cowell_GuestBook.Controllers
         // POST: Article/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+        public ActionResult DeleteConfirmed(int id) {
             ARTICLE aRTICLE = db.ARTICLE.Find(id);
             db.ARTICLE.Remove(aRTICLE);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
